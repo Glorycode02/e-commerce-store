35d6ca5ace454c8e58d63ef2d8ec06507e56039d
Glorycode02 <glorycode02@gmail.com>
2024-05-19 16:35:21 +0200

    commit

--- app/admin/_actions/products.ts
+++ app/admin/_actions/products.ts
@@ -56,4 +56,7 @@ export async function deleteProduct(id: string) {
     const product = await prisma.product.delete({ where: { id } })
 
     if (product === null) return notFound()
+
+    await fs.unlink(product.filePath)
+    await fs.unlink(`public${product.imagePath}`)
 }
--- app/admin/products/_components/ProductActions.tsx
+++ app/admin/products/_components/ProductActions.tsx
@@ -3,17 +3,22 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { useTransition } from "react"
 import { deleteProduct, toggleProAvail } from "../../_actions/products"
+import { useRouter } from "next/navigation"
+import router from "next/router"
 
 export function ActiveToggleDropdownItem({ id, isAvailableForPurchase }: { id: string, isAvailableForPurchase: boolean }) {
     const [isPending, startTransition] = useTransition()
+    const router = useRouter();
     return (
         <DropdownMenuItem
             disabled={isPending}
             onClick={() => {
                 startTransition(async () => {
                     await toggleProAvail(id, !isAvailableForPurchase)
+                    router.refresh()
                 })
             }}
+            className={isAvailableForPurchase ? "bg-red-600" : "bg-green-600"}
         >
             {isAvailableForPurchase ? "Deactivate" : "Activate"}
         </DropdownMenuItem>
@@ -28,8 +33,10 @@ export function DeleteDropdownItem({ id, disabled }: { id: string, disabled: boo
             onClick={() => {
                 startTransition(async () => {
                     await deleteProduct(id)
+                    router.refresh()
                 })
             }}
+            className="text-red-600"
         >
             Delete
         </DropdownMenuItem>
